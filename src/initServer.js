beb5ff232d77038f20c8c9899acb4373d15c1f5c
Orange <bobrov.d.p@readyforsky.com>
2021-05-18 22:59:16 +0300

    fix

--- src/initServer.js
+++ src/initServer.js
@@ -1,14 +1,7 @@
 //тут всё для создания сервера, результат работы - переменная server
 import { Server } from '@logux/server';
-import { resolve, join } from 'path';
 
 const isProd = process.env.NODE_ENV === 'production';
-
-let credentials = {};
-if (isProd) {
-  credentials.cert = resolve(join(process.env.SSL_CERT_DIR, 'ca-certificates.crt'));
-  // credentials.key = resolve(join(process.env.SSL_CERT_DIR, 'nastavnichestvo.org.key'));
-}
 
 const server = new Server(
   Server.loadOptions(process, {
@@ -16,7 +9,6 @@ const server = new Server(
     port: 31337,
     subprotocol: '1.0.0',
     supports: '1.0.0',
-    ...credentials,
   }),
 );
 
