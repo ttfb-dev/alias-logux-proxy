00781cce6099318ddff7ff388f59374ae35f9ecf
Orange <bobrov.d.p@readyforsky.com>
2021-05-26 01:34:29 +0300

    add client events

--- src/modules/team.js
+++ src/modules/team.js
@@ -34,12 +34,71 @@ const team = (server) => {
       await server.log.add({
         type: 'room/user_joined_team',
         roomId,
+        teamId,
         userId,
       });
 
       ctx.sendBack({
         type: 'room/join_team_success',
       });
+    },
+  });
+  
+  server.type('room/team_leave', {
+    async access(ctx, action, meta) {
+      const roomId = parseInt(action.roomId);
+      const userId = parseInt(ctx.userId);
+      const currentTeam = await roomService.getMyTeam(roomId, userId);
+      //допускаем только тех кто находится в комнате
+      return currentTeam !== null;
+    },
+    async process(ctx, action, meta) {
+      const roomId = parseInt(action.roomId);
+      const userId = parseInt(ctx.userId);
+
+      const result = await teamService.leaveTeam(roomId, userId);
+
+      if (result instanceof ErrorResponse) {
+        ctx.sendBack({
+          type: 'room/leave_team_error',
+          ...result,
+        });
+
+        return;
+      }
+
+      await server.log.add({
+        type: 'room/user_left_team',
+        roomId,
+        teamId,
+        userId,
+      });
+
+      ctx.sendBack({
+        type: 'room/leave_team_success',
+        myTeam: null,
+      });
+    },
+  });
+
+  /** client actions */
+  //событие присоединения к комнате
+  server.type('room/user_joined_team', {
+    access() {
+      return true;
+    },
+    async resend (ctx, action, meta) {
+      return `room/${ action.roomId }`
+    },
+  });
+
+  //событие отключения от комнаты
+  server.type('room/user_left_team', {
+    access() {
+      return true;
+    },
+    async resend (ctx, action, meta) {
+      return `room/${ action.roomId }`
     },
   });
 };
