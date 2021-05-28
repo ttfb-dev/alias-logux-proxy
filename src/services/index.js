fc251e45408646ca1a75d69bd614454c696473ce
Orange <bobrov.d.p@readyforsky.com>
2021-05-29 02:21:10 +0300

    fix typo

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import { RoomService } from './roomService.js';
 import { TeamService } from './teamService.js';
 import { WordService } from './wordService.js';
+import { ProfileService } from './profileService.js';
 
-export { RoomService, TeamService, WordService };
+export { RoomService, TeamService, WordService, ProfileService };
