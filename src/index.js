e860a105c5e3b9287491f1b251671349d457928a
Ilia Klimov <therapyspb@yandex.ru>
2021-06-14 13:12:00 +0300

    фикс

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-export { base, game, profile, room, team } from './modules/index.js';
+import { base, game, profile, room, team } from './modules/index.js';
 import { server } from './init.js';
 
 dotenv.config();
@@ -9,6 +9,6 @@ base(server);
 game(server);
 profile(server);
 room(server);
-teams(server);
+team(server);
 
 server.listen();
