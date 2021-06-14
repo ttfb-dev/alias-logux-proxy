8261b8f118dfc2ad2040413bd9cee1801684aa71
Ilia Klimov <therapyspb@yandex.ru>
2021-06-14 13:06:23 +0300

    вернул инициализацию

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv';
 
+export { base, game, profile, room, team } from './modules/index.js';
 import { server } from './init.js';
 
 dotenv.config();
 
-server.autoloadModules();
+base(server);
+game(server);
+profile(server);
+room(server);
+teams(server);
+
 server.listen();
