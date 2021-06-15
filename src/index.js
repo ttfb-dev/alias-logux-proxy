bfc3db821cb8b309fcbc9af2e8d11c42c6b5eb00
Ilia Klimov <therapyspb@yandex.ru>
2021-06-15 03:13:40 +0300

    эксперимент с автолоадом модулей

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
 
-import { base, game, profile, room, team } from './modules';
+// import { base, game, profile, room, team } from './modules';
 import { server } from './init';
 
 dotenv.config();
 
-base(server);
-game(server);
-profile(server);
-room(server);
-team(server);
+// base(server);
+// game(server);
+// profile(server);
+// room(server);
+// team(server);
 
+server.autoloadModules();
 server.listen();
--- src/modules/index.js
+++
@@ -1,5 +0,0 @@
-export { default as base } from './base';
-export { default as game } from './game';
-export { default as profile } from './profile';
-export { default as room } from './room';
-export { default as team } from './team';
