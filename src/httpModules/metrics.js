a07ae51fa5da4625d25ff6ca18952e74d97a7d0d
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-08 23:21:37 +0300

    add array

--- src/httpModules/metrics.js
+++ src/httpModules/metrics.js
@@ -3,8 +3,10 @@ import { logger } from '../libs';
 const metrics = (httpServer) => {
   httpServer.post('/metrics', async (request, response) => {
     try {
-      const { event, userId, ...data } = request.body;
-      await logger.metrics(event ? event : '', userId, data);
+      const rows = request.body;
+      await rows.forEach(async ({ event, userId, ...data }) => {
+        await logger.metrics(event ? event : '', userId, data);
+      });
       response.status(200).send();
     } catch ({ message }) {
       await logger.critical(message);
