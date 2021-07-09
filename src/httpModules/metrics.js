aac5197938acb399705e9f62d047110b16ab7b20
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-09 19:50:49 +0300

    add json parse text body

--- src/httpModules/metrics.js
+++ src/httpModules/metrics.js
@@ -3,9 +3,9 @@ import { logger } from '../libs';
 const metrics = (httpServer) => {
   httpServer.post('/metrics', async (request, response) => {
     try {
-      const rows = request.body;
+      const rows = JSON.parse(request.body);
       await rows.forEach(async ({ event, userId, ...data }) => {
-        await logger.metrics(event ? event : '', userId, data);
+        await logger.metrics(event, userId, data);
       });
     } catch ({ message }) {
       await logger.critical(message);
