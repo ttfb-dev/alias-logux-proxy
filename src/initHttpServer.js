15f9f75595be1cf8eced1a7b9545ceececcc5973
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-09 20:06:24 +0300

    debug

--- src/initHttpServer.js
+++ src/initHttpServer.js
@@ -1,8 +1,9 @@
-import { text } from 'body-parser';
+import { json, text } from 'body-parser';
 import express from 'express';
 
 const httpServer = express();
 httpServer.use(text);
+httpServer.use(json);
 const port = 80;
 
 export { httpServer, port };
