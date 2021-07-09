a7ac99540c0c95094c1de54edc7ce21b9a7ab8c2
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-09 20:27:15 +0300

    change platform and add ua

--- src/httpModules/metrics.js
+++ src/httpModules/metrics.js
@@ -1,11 +1,19 @@
+import parser from 'ua-parser-js';
+
 import { logger } from '../libs';
 
 const metrics = (httpServer) => {
   httpServer.post('/metrics', async (request, response) => {
     try {
+      const { browser, os, device } = parser(request.headers['user-agent']);
       const rows = JSON.parse(request.body);
       rows.forEach(async ({ event, userId, ...data }) => {
-        await logger.metrics(event, userId, data);
+        await logger.execMetrics('vk-miniapp', event, userId, {
+          browser,
+          os,
+          device,
+          ...data,
+        });
       });
     } catch ({ message }) {
       await logger.critical(message, { body: request.body });
