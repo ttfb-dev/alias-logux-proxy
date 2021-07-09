45831d8353ae2e0ba5adf47e7ab8d783580bb98c
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-07-09 20:08:48 +0300

    fix bodyparser

--- src/initHttpServer.js
+++ src/initHttpServer.js
@@ -1,5 +1,7 @@
-import { json, text } from 'body-parser';
+import bodyParser from 'body-parser';
 import express from 'express';
+
+const { json, text } = bodyParser;
 
 const httpServer = express();
 httpServer.use(text);
