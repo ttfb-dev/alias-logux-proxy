32a6871855f8c33f951990f7ad197fa2d31dec2c
Orange <bobrov.d.p@readyforsky.com>
2021-08-31 16:11:39 +0300

    rm bodyparser

--- src/initHttpServer.js
+++ src/initHttpServer.js
@@ -1,11 +1,8 @@
-import bodyParser from 'body-parser';
 import express from 'express';
 
-const { text, json } = bodyParser;
-
 const httpServer = express();
-httpServer.use(text());
-httpServer.use(json());
+httpServer.use(express.text());
+httpServer.use(express.json());
 const port = 80;
 
 export { httpServer, port };
